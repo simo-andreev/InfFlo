e1c3d181ac0d32e1c23db7c28c4f3ef8d045bf21
Simeon Andreev <simeon.andreev@softwaregroup.com>
2021-10-30 01:30:00 +0300

    chore: MISC - Initial commit

---
+++ InfFlo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace InfFlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
